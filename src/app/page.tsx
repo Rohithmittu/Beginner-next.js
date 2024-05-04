ccbc7d3c255a61a3a3162bb4f6b9d00b9118bf91
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-05-04 18:16:44 +0530

    exploring acertinity UI library and implemented some features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Chooseus from "@/components/Chooseus";
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
+      <Chooseus />
     </main>
   );
 }
